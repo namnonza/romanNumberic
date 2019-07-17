46bfc41c5eccca332e246798817dae07aaf80754
Gonk <Gonk>
2019-07-17 21:27:42 +0700

    add case 21-29 is input

--- src/test/RomanNumbericTest.java
+++ src/test/RomanNumbericTest.java
@@ -118,4 +118,34 @@ public class RomanNumbericTest {
         String actual = count.romanNumberic(20);
         assertEquals("XX", actual);
     }
+
+    @Test
+    public void itShouldXXIIIWhenInputIS23 () {
+        String actual = count.romanNumberic(23);
+        assertEquals("XXIII", actual);
+    }
+
+    @Test
+    public void itShouldXXIVWhenInputIS24 () {
+        String actual = count.romanNumberic(24);
+        assertEquals("XXIV", actual);
+    }
+
+    @Test
+    public void itShouldXXVWhenInputIS25 () {
+        String actual = count.romanNumberic(25);
+        assertEquals("XXV", actual);
+    }
+
+    @Test
+    public void itShouldXXVIIWhenInputIS27 () {
+        String actual = count.romanNumberic(27);
+        assertEquals("XXVII", actual);
+    }
+
+    @Test
+    public void itShouldXXIXWhenInputIS29 () {
+        String actual = count.romanNumberic(29);
+        assertEquals("XXIX", actual);
+    }
 }
